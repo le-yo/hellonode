ea1b4d6e9e7a730fe4bfbc70a986844c666724eb
Le-Yo <le-yo@le-yo.com>
2017-05-15 19:54:34 +0300

    initial commit hello node

---
+++ db.js
@@ -0,0 +1,4 @@
+// db.js
+var mongoose = require('mongoose');
+//mongoose.connect('mongodb://yourDatabaseURI');
+mongoose.connect('mongodb://hellonode:hellonode2017@ds139791.mlab.com:39791/hellonode');
