25223320817d687be9944d20564c7cad813ecc8c
Le-Yo <le-yo@le-yo.com>
2017-05-15 20:20:56 +0300

    fixing stuff

--- user/UserController.js
+++ user/UserController.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: true }));
-var User = require('./User');
+var User = require('./user');
 // ADD THIS PART
 // CREATES A NEW USER
 router.post('/', function (req, res) {
